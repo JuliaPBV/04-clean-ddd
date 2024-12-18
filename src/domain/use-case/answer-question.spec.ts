69dd4eebc45a32ce8d243bcedead389e70dadf3a
Julia Pereira <vital.pereira.julia@gmail.com>
2024-12-18 16:07:45 -0300

    Configuração do Eslint

--- src/domain/use-case/answer-question.spec.ts
+++ src/domain/use-case/answer-question.spec.ts
@@ -1,21 +1,19 @@
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswerRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  },
-};
+  create: async (_answer: Answer) => {},
+}
 
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
   const answer = await answerQuestion.execute({
-    questionId: "1",
-    instructorId: "2",
-    content: "Nova resposta",
-  });
+    questionId: '1',
+    instructorId: '2',
+    content: 'Nova resposta',
+  })
 
-  expect(answer.content).toEqual("Nova resposta");
-});
+  expect(answer.content).toEqual('Nova resposta')
+})

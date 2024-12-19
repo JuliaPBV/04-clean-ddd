4645a943a782420290b50667b8a8a2bd5e2dd26c
Julia Pereira <vital.pereira.julia@gmail.com>
2024-12-19 17:25:53 -0300

    Caso de uso Listar comentários da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationsParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/entreprise/entities/question-comment";
 
@@ -14,6 +15,14 @@ export class InMemoryQuestionCommentsRepository
     }
 
     return questionComment;
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationsParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() == questionId)
+      .slice((page - 1) * 20, page * 20);
+
+    return questionComments;
   }
 
   async create(questionComment: QuestionComment) {

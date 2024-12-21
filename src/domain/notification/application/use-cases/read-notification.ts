724e99f89714339c9c14b1201729806a423bfc2a
Julia Pereira <vital.pereira.julia@gmail.com>
2024-12-21 18:50:53 -0300

    Enviando notificação no subscriber

--- src/domain/notification/application/use-cases/read-notification.ts
+++ src/domain/notification/application/use-cases/read-notification.ts
@@ -4,12 +4,12 @@ import { NotificationsRepository } from "../repositories/notifications-repositor
 import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
 import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
 
-interface ReadNotificationUseCaseRequest {
+export interface ReadNotificationUseCaseRequest {
   recipientId: string;
   notificationId: string;
 }
 
-type ReadNotificationUseCaseResponse = Either<
+export type ReadNotificationUseCaseResponse = Either<
   ResourceNotFoundError | NotAllowedError,
   {
     notification: Notification;
